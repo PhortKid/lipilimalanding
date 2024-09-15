a24158d906a81abef3837c337e383ca5b2ad52d8
PhortKid <middlephort@gmail.com>
2024-09-15 16:41:45 +0300

    update database

--- lplmtwr (5).sql
+++ lplmtwr (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 14, 2024 at 10:54 AM
+-- Generation Time: Sep 15, 2024 at 03:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,13 +36,6 @@ CREATE TABLE `budgets` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `budgets`
---
-
-INSERT INTO `budgets` (`id`, `category_id`, `budget_amount`, `start_date`, `end_date`, `created_at`, `updated_at`) VALUES
-(2, 3, 8000.00, '2024-08-30', '2024-08-30', '2024-08-29 15:05:39', '2024-08-29 15:05:39');
 
 -- --------------------------------------------------------
 
@@ -108,14 +101,6 @@ CREATE TABLE `expenses` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `expenses`
---
-
-INSERT INTO `expenses` (`id`, `description`, `amount`, `date`, `category_id`, `status`, `created_at`, `updated_at`) VALUES
-(2, 'rewgtrwegrwege', 700.00, '2024-08-28', 3, 'paid', '2024-08-29 13:56:38', '2024-08-29 13:56:38'),
-(3, '4etrf43', 700.00, '2024-08-22', 3, 'paid', '2024-08-29 13:58:23', '2024-08-29 13:58:23');
-
 -- --------------------------------------------------------
 
 --
@@ -128,14 +113,6 @@ CREATE TABLE `expense_categories` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `expense_categories`
---
-
-INSERT INTO `expense_categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(3, 'Salary', '2024-08-29 09:09:46', '2024-08-29 09:09:46'),
-(4, 'Maintainance', '2024-08-29 13:44:43', '2024-08-29 13:44:43');
 
 -- --------------------------------------------------------
 
@@ -272,25 +249,6 @@ CREATE TABLE `online_booking` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `online_booking`
---
-
-INSERT INTO `online_booking` (`id`, `firstname`, `lastname`, `email`, `phone_number`, `checkin_date`, `checkout_date`, `number_of_days`, `number_of_guest`, `room_number`, `total_amount`, `booking_id`, `is_marked`, `created_at`, `updated_at`) VALUES
-(1, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-26', '2024-10-08', '12', '2', '4', '985000.00', '89651', 'no', '2024-09-13 11:03:38', '2024-09-13 11:03:38'),
-(2, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-26', '2024-10-08', '12', '2', '4', '985000.00', '40531', 'no', '2024-09-13 11:03:54', '2024-09-13 11:03:54'),
-(3, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-25', '2024-09-28', '3', '2', '4', '265000.00', '679092', 'no', '2024-09-13 11:07:13', '2024-09-13 11:07:13'),
-(4, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-25', '2024-09-28', '3', '2', '4', '265000.00', '120998', 'no', '2024-09-13 11:07:47', '2024-09-13 11:07:47'),
-(5, 'phort', 'chrispin', 'middlephort@gmail.com', '+10787753939', '2024-09-25', '2024-09-28', '3', '2', '4', '265000.00', '174308', 'no', '2024-09-13 11:20:30', '2024-09-13 11:20:30'),
-(6, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-27', '2024-09-30', '3', '2', '4', '265000.00', '511130', 'no', '2024-09-13 11:21:56', '2024-09-13 11:21:56'),
-(7, 'phort', 'chrispin', 'middlephort@gmail.com', '+10787753939', '2024-09-27', '2024-10-01', '4', '2', '4', '345000.00', '515418', 'no', '2024-09-13 11:23:02', '2024-09-13 11:23:02'),
-(8, 'phort', 'chrispin', 'middlephort@gmail.com', '+10787753939', '2024-09-27', '2024-09-30', '3', '2', '4', '265000.00', '483800', 'no', '2024-09-13 11:29:14', '2024-09-13 11:29:14'),
-(9, 'phort', 'chrispin', 'middlephort@gmail.com', '+10787753939', '2024-09-27', '2024-09-30', '3', '2', '4', '265000.00', '482856', 'no', '2024-09-13 11:29:22', '2024-09-13 11:29:22'),
-(10, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-27', '2024-10-01', '4', '2', '4', '345000.00', '116438', 'no', '2024-09-13 11:33:50', '2024-09-13 11:33:50'),
-(11, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-27', '2024-10-01', '4', '2', '4', '345000.00', '269304', 'no', '2024-09-13 11:34:12', '2024-09-13 11:34:12'),
-(12, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-26', '2024-09-30', '4', '2', '7', '625000.00', '110099', 'no', '2024-09-14 05:13:00', '2024-09-14 05:13:00'),
-(13, 'phort', 'chrispin', 'middlephort@gmail.com', '+1787753939', '2024-09-26', '2024-09-30', '4', '2', '7', '625000.00', '444297', 'no', '2024-09-14 05:13:08', '2024-09-14 05:13:08');
-
 -- --------------------------------------------------------
 
 --
@@ -364,9 +322,9 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`id`, `room_number`, `room_type`, `description`, `status`, `created_at`, `updated_at`, `cost`) VALUES
-(4, '1A', 'standard', NULL, 'available', '2024-08-31 04:23:57', '2024-09-05 13:30:43', '80000'),
-(6, '1b', 'deluxe', NULL, 'available', '2024-09-14 04:37:35', '2024-09-14 04:37:35', '100000'),
-(7, '1c', 'executive', NULL, 'available', '2024-09-14 04:43:29', '2024-09-14 04:43:29', '150000');
+(8, 'standard 1', 'standard', NULL, 'available', '2024-09-15 09:39:19', '2024-09-15 09:39:19', '60000'),
+(9, 'deluxe 1', 'standard', NULL, 'available', '2024-09-15 09:39:30', '2024-09-15 09:39:30', '60000'),
+(10, 'executive 1', 'executive', NULL, 'available', '2024-09-15 09:39:43', '2024-09-15 09:39:43', '1200000');
 
 -- --------------------------------------------------------
 
@@ -387,9 +345,9 @@ CREATE TABLE `room_type` (
 --
 
 INSERT INTO `room_type` (`id`, `name`, `cost`, `created_at`, `updated_at`) VALUES
-(3, 'standard', '80000', '2024-08-31 04:23:22', '2024-08-31 04:23:22'),
-(4, 'deluxe', '100000', '2024-09-14 04:30:41', '2024-09-14 04:30:41'),
-(5, 'executive', '150000', '2024-09-14 04:30:58', '2024-09-14 04:30:58');
+(3, 'standard', '60000', '2024-08-31 04:23:22', '2024-08-31 04:23:22'),
+(4, 'deluxe', '80000', '2024-09-14 04:30:41', '2024-09-14 04:30:41'),
+(5, 'executive', '1200000', '2024-09-14 04:30:58', '2024-09-14 04:30:58');
 
 -- --------------------------------------------------------
 
@@ -424,6 +382,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('0Y12vmoUeBkfcsYHZfxowEoYXegnVzVQS6qjB0tg', 1, '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiUUtiVEdpejlQT3VDM0UwY0hwcjVqRkFiUnA2MWo5VWNvbVlzQkJiTCI7czoxODoiZmxhc2hlcjo6ZW52ZWxvcGVzIjthOjA6e31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjI2OiJodHRwOi8vMTI3LjAuMC4xOjgwODAvZGFzaCI7fX0=', 1726404452),
 ('TdZuHqUXMAqVYuZPZ19thDX3CeWjeCQvgmmtEIPW', 1, '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiV0s4WUdCbWFCN1k3WklDSTlpQW5RdmdNaFBSVjVGdHRlbzRUZnhJRSI7czoxODoiZmxhc2hlcjo6ZW52ZWxvcGVzIjthOjA6e31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czo0MzoiaHR0cDovLzEyNy4wLjAuMTo4MDgwL2Rhc2gvZ3Vlc3RfbWFuYWdlbWVudCI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1726301272);
 
 -- --------------------------------------------------------
@@ -470,10 +429,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `phone_number`, `profile_image`, `email`, `gender`, `email_verified_at`, `role`, `password`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Phort', NULL, 'Chrispin', '0787753939', '1724088548.jpeg', 'middlephort@gmail.com', 'Male', NULL, 'Admin', '$2y$12$3rZSnBj0XwKuWAAUz1S/OOdNOzztTUBGVrPeivJSqWAPuZzc1gs12', 'active', 'qVeU1b12LKcxbgKEeTv39fjkB4C3Vegr5u3FkVyufRNdRjcNpgJQnAhvfFCm', '2024-08-19 10:26:43', '2024-09-14 04:22:27'),
-(5, 'hamis', 'dee', 'huiopi', '0787753939', NULL, 'itclub2426@gmail.com', 'Male', NULL, 'Admin', '$2y$12$aN1kh64IUf.iFp3tEMM1qeVOVGQlqW7lIpiOU3T8vZhEAoPaWso7e', 'active', NULL, '2024-08-28 05:19:23', '2024-09-02 03:46:48'),
-(23, 'phort', 'trhrh', 'majaliwa', '0755445889', NULL, 'itclub2429@gmail.com', 'Male', NULL, 'Admin', '$2y$12$oHTV6TvnRRIKZd8kyWfhJ.tX.jE2jhVpRKHSqMVt8upuwk0R9WlRy', 'active', NULL, '2024-09-02 03:47:02', '2024-09-02 08:54:50'),
-(24, 'phort', 'trhrh', 'majaliwa', '0787753939', NULL, 'itclub2425@gmail.com', 'Male', NULL, 'Admin', '$2y$12$vX9DwRUpcpaf/L5WkzMkz.iDRpOs.9tZP3MqhUGhlEQG27dmE/BZ.', 'active', 'A8i6d4PF5Ox3K8UHIAAK9UuVZKysEQ1epkdDiksjjSlQUszzv8xu8D7FtNoj', '2024-09-02 08:55:03', '2024-09-05 07:56:21');
+(1, 'Phort', NULL, 'Chrispin', '0787753939', '1724088548.jpeg', 'middlephort@gmail.com', 'Male', NULL, 'Admin', '$2y$12$3rZSnBj0XwKuWAAUz1S/OOdNOzztTUBGVrPeivJSqWAPuZzc1gs12', 'active', 'qVeU1b12LKcxbgKEeTv39fjkB4C3Vegr5u3FkVyufRNdRjcNpgJQnAhvfFCm', '2024-08-19 10:26:43', '2024-09-14 04:22:27');
 
 --
 -- Indexes for dumped tables
@@ -674,7 +630,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `online_booking`
 --
 ALTER TABLE `online_booking`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -686,13 +642,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `room_type`
